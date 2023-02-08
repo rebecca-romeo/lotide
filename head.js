269530fb0fe7b7b39bb439d81f212ea3e7c34ab5
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-02-07 20:00:58 -0500

    Implement head challenge

---
+++ head.js
@@ -0,0 +1,24 @@
+
+
+let array = [1, 40, 9]
+function head(arr) {
+  return arr[0]
+}
+
+
+// console.log(head(array))
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  (actual === expected) ?
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`) :
+    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
+};
+
+// TEST CODE
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Bye");
+assertEqual(head([5,6,7]), 5);
+assertEqual(head([5,6,7]), 6);
+assertEqual(head([5]), 5);
+assertEqual(head([]), 45);
