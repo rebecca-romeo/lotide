389b97d8d24d2d06757d3cc734c3cd098b2809bc
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-02-09 14:28:34 -0500

    eqArrays challenge

--- eqArrays.js
+++ eqArrays.js
@@ -11,11 +11,10 @@ function eqArrays(array1, array2) {
       return false;
     }
   }
-
   console.log('true');
   return true;
-
 }
+
 eqArrays([1, 2, 3], [1, 2, 3]); // => true
 eqArrays([1, 2, 3], [3, 2, 1]); // => false
 
