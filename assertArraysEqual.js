0c8451111cd152fc76f76284e3b9454c2eceb904
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-02-11 15:22:03 -0500

    assertArraysEqual function challenge

---
+++ assertArraysEqual.js
@@ -0,0 +1,24 @@
+function eqArrays(array1, array2) {
+  // loop through the arrays
+  for (let i = 0; i < array1.length; i++) {
+    // Check if indexes do not match
+    if (array1[i] !== array2[i]) {
+      console.log('false');
+      return false;
+    }
+  }
+  console.log('true');
+  return true;
+}
+
+// Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
+const assertArraysEqual = function(array1, array2) {
+  (eqArrays(array1, array2)) ? console.log(`✅✅✅ Assertion passed: ${array1} === ${array2}`) :
+  console.log(`🛑🛑🛑 Assertion failed: ${array1} !== ${array2}`);
+}
+
+const array1 = [1, 2, 3]
+const array2 = [1, 2]
+
+assertArraysEqual(array1, array2);
+assertArraysEqual(array1, array2);
