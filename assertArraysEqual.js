fc8b280f4c4e2e5c1745226a755b9af1778dc866
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-02-11 15:34:20 -0500

    eslint

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -14,11 +14,10 @@ function eqArrays(array1, array2) {
 // Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
 const assertArraysEqual = function(array1, array2) {
   (eqArrays(array1, array2)) ? console.log(`✅✅✅ Assertion passed: ${array1} === ${array2}`) :
-  console.log(`🛑🛑🛑 Assertion failed: ${array1} !== ${array2}`);
-}
+    console.log(`🛑🛑🛑 Assertion failed: ${array1} !== ${array2}`);
+};
 
-const array1 = [1, 2, 3]
-const array2 = [1, 2]
+const array1 = [1, 2, 3];
+const array2 = [1, 2];
 
-assertArraysEqual(array1, array2);
 assertArraysEqual(array1, array2);
