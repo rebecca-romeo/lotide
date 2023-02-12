08eafe17cdb0a311ea9867a305022975c151516e
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-02-11 21:36:00 -0500

    findKeyByValue function complete

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,3 +1,7 @@
+// ✅ assignment complete
+  // ✅ findKeyByValue function
+  // ✅ assertEqual test
+
 const assertEqual = function(actual, expected) {
   (actual === expected) ?
     console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`) :
@@ -6,16 +10,20 @@ const assertEqual = function(actual, expected) {
 
 
 // Function to check object for value ? return the first key that contains the value : undefined
-const findKeyByValue = function (object, value) {
+const findKeyByValue = function(object, value) {
+  // Object.keys creates an array of all the keys in an object
+  // You can then loop the array using for of to check each key
   for (const key of Object.keys(object)) {
     if (object[key] === value) {
-      console.log(key)
+      console.log(key);
       return key;
     }
   }
-  console.log("undefined")
+  console.log("undefined");
   return undefined;
-}
+};
+
+
 const bestTVShowsByGenre = {
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
