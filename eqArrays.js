dc2fd242e2c3ec70f8bcb732fabf6100e185cfb3
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-02-11 19:40:54 -0500

    eqArrays function complete

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,16 @@
+// ✅ assignment complete
+  // ✅ eqArrays function
+  // ✅ assertEqual tests
+
 const assertEqual = function(actual, expected) {
   (actual === expected) ?
     console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`) :
     console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
 };
 
+// Function that checks the index of 2 arrays to see if they match
 function eqArrays(array1, array2) {
+  // loop through the arrays
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       console.log('false');
