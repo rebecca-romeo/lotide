697fb41497fb6ee1b9a5c444c1caa428d65d5ae4
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-02-11 19:23:53 -0500

    middle function challenge

---
+++ middle.js
@@ -0,0 +1,26 @@
+// ✅ middle function
+// 🛑 assertArraysEqual & eqArrays
+
+const middle = function(array) {
+  let middleOfArray = [];
+  if (array.length <= 2) {
+    return middleOfArray;
+  }
+
+  if (array.length % 2 === 0) {
+    middleOfArray.push(array[Math.floor((array.length - 1) / 2)]);
+    middleOfArray.push(array[Math.floor(array.length / 2)]);
+  }
+
+
+  if (array.length % 2 === 1) {
+    return middleOfArray = array[Math.floor(array.length / 2)];
+  }
+  return middleOfArray;
+};
+
+
+console.log(middle([1, 2, 3, 4, 5, 6, 7, 8]));
+console.log(middle([1, 2, 3, 4, 5, 6, 7]));
+console.log(middle([1, 2, 3, 4]));
+console.log(middle([1, 2]));
