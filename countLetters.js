d1e8182fd13c308eee50a6f753709679a1cc3e36
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-02-11 21:31:46 -0500

    countLetters function complete

--- countLetters.js
+++ countLetters.js
@@ -1,15 +1,24 @@
+// ✅ assignment complete
+  // ✅ countLetters function
+  // ✅ assertEqual tests
+
 const assertEqual = function(actual, expected) {
   (actual === expected) ?
     console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`) :
     console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
 };
 
+// A function to count all the letters in a string and return how many of each letter there are
 const countLetters = function(string) {
+  // object to hold all the key-value pairs of letter/number
   const countedLetters = {};
+  // loop through each letter of the string
   for (const letter of string) {
+    // if that letter is in the object, add 1
     if (countedLetters[letter]) {
       countedLetters[letter] += 1;
     } else {
+      // if  that letter is not found in the object, add the letter name(key) and 1 (value)
       countedLetters[letter] = 1;
     }
   }
@@ -17,7 +26,8 @@ const countLetters = function(string) {
 };
 
 
+// The string that is being looped
+const result = countLetters("testing this");
 
-const result = countLetters("test");
-
-assertEqual(result["t"], 2);
+// check to see if the amount of t's === 2
+assertEqual(result["t"], 3);
