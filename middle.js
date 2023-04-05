bf308ad71c6d95e00cf596fd419ac16e59e2c009
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-04-05 18:21:38 -0400

    refactored, added comments, eslint

--- middle.js
+++ middle.js
@@ -18,7 +18,7 @@ const assertArraysEqual = function(actual, expected) {
   return (eqArrays(actual, expected)) ?
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) :
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} `);
-}
+};
 
 
 // FUNCTION: middle checks the middle numbers of an array
@@ -28,15 +28,18 @@ const assertArraysEqual = function(actual, expected) {
 // even numbers = [1, 2]
 
 const middle = function(array) {
+  // Arrays with 2 or less items, there is no middle = []
   if (array.length < 3) return [];
 
+  // Arrays with even numbers, middle = [x, x]
   if (array.length % 2 === 0) {
-    return [array[((array.length / 2) - 1)], array[array.length / 2]]
-  } else {
-    return [array[Math.floor(array.length / 2)]]
+    return [array[((array.length / 2) - 1)], array[array.length / 2]];
   }
 
-}
+  // Arrays with odd numbers, middle = [x]
+  return [array[Math.floor(array.length / 2)]];
+
+};
 
 
 
