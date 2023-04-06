d0ccd9e23bff84f41f85fa3596f37318c0390ecb
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-04-06 13:12:53 -0400

    countLetters function

---
+++ countLetters.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const countLetters = function(sentance) {
+  const results = {};
+
+  for (const letter of sentance) {
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+
+  console.log(results);
+  return results;
+};
+
+const sentance = "hello";
+const result1 = countLetters(sentance);
+assertEqual(result1["h"], 1);
+assertEqual(result1["e"], 1);
+assertEqual(result1["l"], 2);
+assertEqual(result1["o"], 1);
