659ad4066973d0e7a0f835de43e2189df0b03395
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-04-06 13:05:23 -0400

    eslint

--- countOnly.js
+++ countOnly.js
@@ -1,4 +1,4 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   }
@@ -7,22 +7,22 @@ const assertEqual = function (actual, expected) {
 
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
-const countOnly = function (allItems, itemsToCount) {
-  const results = {}
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
 
   for (const item of allItems) {
 
     if (itemsToCount[item]) {
       if (results[item]) {
-        results[item] += 1
+        results[item] += 1;
       } else {
-        results[item] = 1
+        results[item] = 1;
       }
     }
   }
-  console.log(results)
+  console.log(results);
   return results;
-}
+};
 
 
 const firstNames = [
