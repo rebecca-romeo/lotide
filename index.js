7de98979268a9a634fa5141aa85b2f9ff7cfe04e
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-04-10 22:17:51 -0400

    Exporting All Functions

---
+++ index.js
@@ -0,0 +1,33 @@
+const eqArrays = require('./eqArrays');
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+const eqObjects = require('./eqObjects');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const without = require('./without');
+
+module.exports = {
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  letterPositions: letterPositions,
+  map: map,
+  without: without
+};
