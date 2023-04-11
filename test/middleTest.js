81f206195fdeb0405ec8a4067223e6a682fd413d
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-04-10 21:03:59 -0400

    mocha/chai testing for middle function

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,24 +1,38 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
+// const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
 
 // TESTS
 
 // For arrays with one or two elements, there is no middle. Return an empty array.
-// middle([1]) // => []
-// middle([1, 2]) // => []
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
+// assertArraysEqual(middle([1]), []);
+// assertArraysEqual(middle([1, 2]), []);
 
-// For arrays with odd number of elements, an array containing a single middle element should be returned.
-// middle([1, 2, 3]) // => [2]
-// middle([1, 2, 3, 4, 5]) // => [3]
-assertArraysEqual(middle([1, 17, 3]), [17]);
-assertArraysEqual(middle([1, 2, 10, 4, 5]), [10]);
+// // For arrays with odd number of elements, an array containing a single middle element should be returned.
+// assertArraysEqual(middle([1, 17, 3]), [17]);
 
-// For arrays with an even number of elements, an array containing the two elements in the middle should be returned
-// middle([1, 2, 3, 4]) // => [2, 3]
-// middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3, 9, 12, 6, 7, 8]), [9, 12]);
+// // For arrays with an even number of elements, an array containing the two elements in the middle should be returned
+// assertArraysEqual(middle([1, 17, 18, 4]), [17, 18]);
 
+
+describe('#middle', () => {
+  // empty array
+  it('returns [] for [1]', () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  // length = 2
+  it('returns [] for [1, 2]', () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  // length = odd
+  it('returns [17] for [1, 17, 3]', () => {
+    assert.deepEqual(middle([1, 17, 3]), [17]);
+  });
+
+  // length = even
+  it('returns [17, 18] for [1, 17, 18, 2]', () => {
+    assert.deepEqual(middle([1, 17, 18, 2]), [17, 18]);
+  });
+});
